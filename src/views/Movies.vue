6e4c11b53888dcb7a8f9e1eb7475ccccc637a89e
Flori <florianpilslservice+gitlab@gmail.com>
2024-12-28 17:10:48 +0100

    Refactor Movies view to use shared component definition and simplify data fetching logic

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -16,35 +16,11 @@
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from 'vue';
-import { generateClient } from 'aws-amplify/data';
+import defineComponent from './Movies.js';
 
-export default defineComponent({
-  name: 'Movies',
-  setup() {
-    const client = generateClient();
-    const movies = ref([]);
-
-    const fetchMovies = async () => {
-      try {
-        const { data, errors } = await client.models.Movie.list();
-        if (errors) {
-          console.error('Error fetching movies:', errors);
-        } else {
-          movies.value = data;
-        }
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
-
-    onMounted(fetchMovies);
-
-    return {
-      movies,
-    };
-  },
-});
+export default {
+  ...defineComponent,
+};
 </script>
 
 <style scoped>
