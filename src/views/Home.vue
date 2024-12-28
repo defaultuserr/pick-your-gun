d42dfc01a6c36b31c7404bbf9fe35c57b3755260
Flori <florianpilslservice+gitlab@gmail.com>
2024-12-28 11:37:50 +0100

    Remove CategoryCard and GunList components; simplify Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,10 @@
 <template>
   <div>
     <h2>Choose a Category</h2>
-    <div class="categories">
-      <CategoryCard v-for="category in categories" :key="category.id" :category="category" />
-    </div>
+
   </div>
 </template>
 
-<script>
-import CategoryCard from "../components/CategoryCard.vue";
 
-export default {
-  components: { CategoryCard },
-  data() {
-    return {
-      categories: [
-        { id: "movies", name: "Movies", description: "Guns from famous movies" },
-        { id: "videogames", name: "Video Games", description: "Weapons from iconic games" },
-        { id: "hunting", name: "Hunting", description: "Best firearms for hunting" },
-      ],
-    };
-  },
-};
-</script>
 
-<style>
-.categories {
-  display: flex;
-  gap: 15px;
-  flex-wrap: wrap;
-  padding: 20px;
-}
-</style>
+
